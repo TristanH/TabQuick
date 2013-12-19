c7feb7b08462e32ecd8397fe6d53c2ea38771b91
Tristan Homsi <tristanhomsi@gmail.com>
2013-12-18 19:45:50 -0500

    Update to version 0.3
    Moved all of the tab saving operations to an event page, allowing the
    script to finish after the user gives their desired folder name and the
    popup window goes away.

--- popup.js
+++ popup.js
@@ -1,59 +1,17 @@
 
-var tabSaver = {
-	
-	groupName: "Cake",
-	
-   //Does all of the required actions of the app
-  begin: function() { 
-  //Returns the ID of the bookmark folder where tabs are saved. Creates the folder if it doesn't exist.
-	 chrome.storage.sync.get('TabQuickID',
-		function(result){
-			if(typeof result.TabQuickID === "undefined") //add in checking if the ID's bookmark exists here
-				tabSaver.createMainFolder();
-			else{
-				console.log("Got tabquickid, it is: " + result.TabQuickID); 
-				console.log(result);
-				tabSaver.saveTabs(result.TabQuickID);
-				}
-		});
-  },
-   
-  createMainFolder: function() {
-	chrome.bookmarks.create({
-						 'parentId' : '1',
-						 'title': 'Saved Tabs'
-                         },
-						 function(result){
-							chrome.storage.sync.set({'TabQuickID' : result.id});
-							console.log('Created tabquick folder and saved in storage, its ID is'+ result.id);
-							tabSaver.saveTabs(result.id);
-						 });
-  },
-  
-  saveTabs: function (folderID){
-	  console.log("Got to saveTabs, folderID is"+ folderID);
-	  chrome.bookmarks.create({
-						'parentId' : folderID,
-						'title' : this.groupName
-						},
-						function(result){ 
-							
-						});
-						
-  }
-  
-};
-
-
-document.onsubmit=function(){
-	tabSaver.groupName = document.forms["form"]["groupName"].value;
-	tabSaver.begin();
-	
-}
-
-/*
 document.addEventListener('DOMContentLoaded', function () {
-    tabSaver.groupName = "testwooh";
-	tabSaver.begin();
+    var form = document.getElementById("form");
+	var nevermind = document.getElementById("nevermind");
+
+	form.addEventListener('submit', function() {
+		chrome.runtime.getBackgroundPage(function(bgWindow){
+											bgWindow.tabSaver.groupName = document.forms["form"]["groupName"].value;
+											bgWindow.tabSaver.begin();
+										});
+		window.close();
+	});
+	nevermind.addEventListener('click', function(){
+		window.close();
+	});
+
 });
-*/
