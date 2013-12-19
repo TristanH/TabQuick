048727d6ebf5b72e206f1e84b07a5fa5c689bbed
Tristan Homsi <tristanhomsi@gmail.com>
2013-12-18 21:51:45 -0500

    Version 1.0 - It works now!
    Updated a bunch of stuff and finally completed the basic functionality
    of the extension, wooh.

--- eventPage.js
+++ eventPage.js
@@ -1,54 +1,68 @@
 var tabSaver = {
-	
-	groupName: "Cake",
-	
-   //Does all of the required actions of the app
-  begin: function() { 
-  //Returns the ID of the bookmark folder where tabs are saved. Creates the folder if it doesn't exist.
-	 chrome.storage.sync.get('TabQuickID',
-		function(result){
-			if(typeof result.TabQuickID === "undefined") //add in checking if the ID's bookmark exists here
-				tabSaver.createMainFolder();
 
-			else{
-				console.log("Got tabquickid, it is: " + result.TabQuickID); 
-				tabSaver.saveTabs(result.TabQuickID);
-				}
-		});
-  },
-   
-  createMainFolder: function() {
-	chrome.bookmarks.create({
-						 'parentId' : '1',
-						 'title': 'Saved Tabs'
-                         },
-						 function(result){
-							chrome.storage.sync.set({'TabQuickID' : result.id});
-							console.log('Created tabquick folder and saved in storage, its ID is '+ result.id);
-							tabSaver.saveTabs(result.id);
-						 });
-  },
-  
-  saveTabs: function (folderID){
-	  console.log("Got to saveTabs, folderID is "+ folderID);
-	  chrome.bookmarks.create({
-						'parentId' : folderID.toString(),
-						'title' : this.groupName
-						}, function(result){
-							chrome.tabs.query({},
-								function(allTabs){
-									tabSaver.addTabsTo(allTabs, result.id);
-								}
-							);
-						}
-					);
-	  ;
-  },
-  
-  addTabsTo: function(allTabs, pID){
+    groupName: "",
 
-	alert("finally made it" + pID);
-  
-  }
-  
+    //Does all of the required actions of the app
+    begin: function () {
+        //Returns the ID of the bookmark folder where tabs are saved. Creates the folder if it doesn't exist.
+        chrome.storage.sync.get('TabQuickID',
+            function (result) {
+                if (typeof result.TabQuickID === "undefined") //add in checking if the ID's bookmark exists here
+                    tabSaver.createMainFolder();
+
+                else {
+                    console.log("Got tabquickid, it is: " + result.TabQuickID);
+                    tabSaver.saveTabs(result.TabQuickID);
+                }
+            });
+    },
+
+    createMainFolder: function () {
+        chrome.bookmarks.create({
+                'parentId': '1',
+                'title': 'Saved Tabs'
+            },
+            function (result) {
+                chrome.storage.sync.set({
+                    'TabQuickID': result.id
+                });
+                console.log('Created tabquick folder and saved in storage, its ID is ' + result.id);
+                tabSaver.saveTabs(result.id);
+            });
+    },
+
+    saveTabs: function (folderID) {
+        console.log("Got to saveTabs, folderID is " + folderID);
+        chrome.bookmarks.create({
+            'parentId': folderID.toString(),
+            'title': this.groupName
+        }, function (result) {
+            chrome.tabs.query({},
+                function (allTabs) {
+                    tabSaver.addTabsTo(allTabs, result.id);
+                }
+            );
+        });;
+    },
+
+    addTabsTo: function (allTabs, pID) {
+
+        tabSaver.createRec(0, allTabs, pID);
+
+    },
+
+    createRec: function (index, allTabs, pID) {
+        if (index >= allTabs.length)
+            return;
+        else
+            chrome.bookmarks.create({
+                'parentId': pID,
+                'title': allTabs[index].title,
+                'url': allTabs[index].url
+            }, function (created) {
+                tabSaver.createRec(index + 1, allTabs, pID);
+            });
+
+    }
+
 };
--- popup.js
+++ popup.js
@@ -1,17 +1,16 @@
-
 document.addEventListener('DOMContentLoaded', function () {
     var form = document.getElementById("form");
-	var nevermind = document.getElementById("nevermind");
+    var nevermind = document.getElementById("nevermind");
 
-	form.addEventListener('submit', function() {
-		chrome.runtime.getBackgroundPage(function(bgWindow){
-											bgWindow.tabSaver.groupName = document.forms["form"]["groupName"].value;
-											bgWindow.tabSaver.begin();
-										});
-		window.close();
-	});
-	nevermind.addEventListener('click', function(){
-		window.close();
-	});
+    form.addEventListener('submit', function () {
+        chrome.runtime.getBackgroundPage(function (bgWindow) {
+            bgWindow.tabSaver.groupName = document.forms["form"]["groupName"].value;
+            bgWindow.tabSaver.begin();
+        });
+        window.close();
+    });
+    nevermind.addEventListener('click', function () {
+        window.close();
+    });
 
 });
